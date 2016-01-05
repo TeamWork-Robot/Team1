2f1987c78417418c24e845abd3c4530c097ad603
Zhanghust <bnng-w@outlook.com>
2016-01-05 10:59:01 +0800

    Update Frame.cpp

--- Robot/Frame.cpp
+++ Robot/Frame.cpp
@@ -1,15 +1,15 @@
 #include "Frame.h"
 using namespace std;
-		Frame::Frame(){}
-	 	Frame::Frame(Point orig,double deg){
-		 	origin=orig;
-		 	degree=deg;
-		 }
-		Point Frame::getPoint(){
-			return origin;
-		}
-		double Frame::getDegree(){
-			return degree;
-		}
-		
+	Frame::Frame(){}
+	 Frame::Frame(Point orig,double deg){
+	 	origin=orig;
+	 	degree=deg;
+	 }
+	Point Frame::getPoint(){
+		return origin;
+	}
+	double Frame::getDegree(){
+		return degree;
+	}
+
 
