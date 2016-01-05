1ab16ecba2506cfdf4e6f2067e8afdc6463f665b
Zhanghust <bnng-w@outlook.com>
2016-01-05 11:01:02 +0800

    Create Point.h

---
+++ Robot/Point.h
@@ -0,0 +1,14 @@
+class Point{
+	private:
+		double x;
+		double y;
+	public: 
+		Point();
+		Point(double a,double b);
+		double getX();
+		double getY();
+		void setX(double a);
+		void setY(double b);
+		
+		
+};
